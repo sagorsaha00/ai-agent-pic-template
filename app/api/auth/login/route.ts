3c64c334dd1d755e9a2e993b0cff5f5916445e77
Sagor Saha <mrartimas24@gmail.com>
2026-08-15 20:26:25 +0600

    c

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -14,9 +14,9 @@ export async function GET() {
     "pages_show_list",//ok
     "public_profile",//ok
     "pages_manage_engagement",
-    "instagram_business_basic",//ok
-    "instagram_content_publish",//ok
-    "instagram_business_content_publish",//ok
+    // "instagram_business_basic",//ok
+    // "instagram_content_publish",//ok
+    // "instagram_business_content_publish",//ok
     "business_management",//ok
    
   ].join(",");
